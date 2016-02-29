33f0de6f0461774b5c9ee2e9493d57d6c9ef6652
maycol elfer <maycol.elfer@gmail.com>
2016-02-29 17:48:46 +0100

    examen parcial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<marquee>
+	<h1>maycol elfer</h1>
+	</marquee>
+		<img src="images.jpg" alt="">
+		<img src="tus-imagenes-de-amor.png" alt="">
+		<marquee>
+		<h1>maycol elfer</h1>
+	</marquee>
+</body>
+</html>
